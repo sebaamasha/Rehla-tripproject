c2631c6e7f0a5cdbee251c2d9fc3bb4995b19777
sebaamasha <sebaama96@gmail.com>
2025-12-21 23:49:17 +0200

    Add React Router and page navigation

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FormPage from "./pages/FormPage";
 import ApiPage from "./pages/ApiPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import "./App.css";
+import { useContext } from "react";
+import { FavoritesContext } from "./context/FavoritesContext";
 
 function App() {
+  const { favorites } = useContext(FavoritesContext);
+
   return (
     <div className="app">
       <header className="main-header">
         <div className="logo">Rehla</div>
+
         <nav className="main-nav">
           <Link to="/">Home</Link>
-          <Link to="/add-story">Form</Link>
+          <Link to="/form">Form</Link>
           <Link to="/api">API Page</Link>
+          <Link to="/favorites">Favorites ({favorites.length})</Link>
         </nav>
       </header>
 
       <main className="page-content">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/add-story" element={<FormPage />} />
+          <Route path="/form" element={<FormPage />} />
           <Route path="/api" element={<ApiPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { FavoritesProvider } from "./context/FavoritesContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
